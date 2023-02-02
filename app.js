af4b3b4b4a22daca3da57058873be5185a38b241
Mahfuz Alam <36205825+Mahfuz60@users.noreply.github.com>
2023-02-02 17:52:29 +0600

    learning-to-interface,unions,type,narrowing,extends

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var user = {
     role: "Software Engineer",
     age: 40
 };
-console.log(user);
 var customer = {
     name: "John Leo",
     age: 30,
@@ -25,11 +24,55 @@ var customer = {
     address: "dhaka,bangladesh",
     price: 30000
 };
-console.log(customer);
+// console.log(customer)
 function login(userData) {
     return userData;
 }
-// console.log(login({ name: 'methon', age: 34, product: 'laptop', price: 123000 }));
-
 var userId = 'Mahfuz1232';
-// console.log(userId);
+;
+;
+var transaction1 = {
+    payerAccountNumber: 100,
+    payeeAccountNumber: 5000
+};
+var transaction2 = {
+    payerAccountNumber: 100,
+    payeeAccountNumber: 5001
+};
+var bankAccount = {
+    accountNumber: 1001,
+    accountHolder: "Mark Wood",
+    isBalance: 50000,
+    isActive: true,
+    transactions: [transaction1, transaction2]
+};
+var book = {
+    name: 'Learn With Python',
+    author: 'Md.Anisul Islam',
+    price: 500,
+    fileSize: '300MB',
+    format: 'pdf',
+    duration: '4.0h'
+};
+var product = {
+    name: 'computer SSD',
+    price: 4200,
+    company: 'samsung',
+    productTax: 10
+};
+function printId(id) {
+    //Narrowing
+    if (typeof id === 'string') {
+        console.log(id.toUpperCase());
+    }
+    else {
+        console.log(id);
+    }
+}
+printId('mahfuz1234');
+printId(1234);
+function getFirstThree(x) {
+    return x.slice(0, 4);
+}
+console.log(getFirstThree('hello'));
+console.log(getFirstThree([1, 2, 3, 4, 5, 6]));
--- app.ts
+++ app.ts
@@ -26,7 +26,7 @@ const user = {
   role: <string>"Software Engineer",
   age: <number>40
 }
-console.log(user)
+// console.log(user)
 
 
 //Type alias
@@ -49,20 +49,152 @@ const customer: Customer = {
 
 }
 
-console.log(customer)
+// console.log(customer)
 
 
 
-function login(userData:Customer):Customer{
+function login(userData: Customer): Customer {
 
   return userData;
 }
 
 // console.log(login({name:'methon',age:34,product:'laptop',price:123000}));
 
-type ID=number|string;
+type ID = number | string;
 
-const userId:ID='Mahfuz1232';
-console.log(userId)
+const userId: ID = 'Mahfuz1232';
+// console.log(userId)
 
 
+//interface
+interface Transaction {
+  payerAccountNumber: number;
+  payeeAccountNumber: number;
+
+};
+
+interface BankAccount {
+  accountNumber: number;
+  accountHolder: string;
+  isBalance: number;
+  isActive: boolean;
+  transactions: Transaction[];
+
+};
+
+const transaction1: Transaction = {
+  payerAccountNumber: 100,
+  payeeAccountNumber: 5000
+  
+
+};
+const transaction2:Transaction = {
+  payerAccountNumber: 100,
+  payeeAccountNumber: 5001
+ 
+
+};
+
+const bankAccount: BankAccount = {
+  accountNumber: 1001,
+  accountHolder: "Mark Wood",
+  isBalance: 50000,
+  isActive: true,
+  transactions: [transaction1,transaction2]
+
+
+};
+
+// console.log(bankAccount.transactions)
+
+
+
+
+//extends
+
+interface Book{
+  name:string;
+  author:string;
+  price:number;
+}
+
+interface EBook extends Book{ //access to Book interface automatically
+  // name:string;
+  // author:string;
+  // price:number;
+  fileSize:string;
+  format:string;
+
+}
+interface AudioBook extends EBook{ //access to EBook automatically
+  // name:string;
+  // author:string;
+  // price:number;
+  // fileSize:string;
+  // format:string;
+  duration:string
+
+}
+
+
+const book:AudioBook={
+  name:'Learn With Python',
+  author:'Md.Anisul Islam',
+  price:500,
+  fileSize:'300MB',
+  format:'pdf',
+  duration:'4.0h'
+
+}
+
+// console.log(book);
+
+
+
+//two interface Marge
+
+interface Product{
+  name:string;
+  price:number;
+  company:string;
+
+}
+interface Product{
+  productTax:number;
+}
+
+const product:Product={
+  name:'computer SSD',
+  price:4200,
+  company:'samsung',
+  productTax:10
+
+}
+
+// console.log(product);
+
+
+
+//Unions
+type userId=number|string;
+
+function printId(id:userId){
+
+  //Narrowing
+  if(typeof id==='string'){
+    // console.log(id.toUpperCase());
+  }else{
+    //  console.log(id);
+
+  }
+ 
+}
+printId('mahfuz1234');
+ printId(1234)
+
+ function getFirstThree(x:string|number[]){
+  return x.slice(0,4);
+
+ }
+//  console.log(getFirstThree('hello'))
+//  console.log(getFirstThree([1,2,3,4,5,6]))

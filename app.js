072fdd81fcf98bbb7a16d3e25fc174103d3c4d74
Mahfuz Alam <36205825+Mahfuz60@users.noreply.github.com>
2023-02-02 15:41:35 +0600

    try-to-typescript

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-/*let num1=3;
-let num2=4;
-function calculate(num1:number,num2:number){
-  return num1+num2;
-
-}
-console.log(calculate(num1,num2));
-*/
+// let num1=3;
+// let num2=4;
+// function calculate(num1:number,num2:number){
+//   return num1+num2;
+// }
+// console.log(calculate(num1,num2));
 function getTotal(numbers) {
     return numbers.reduce(function (acc, curr) {
         return acc + curr;
     }, 0);
 }
-console.log(getTotal([3, 4, 5, 6, 7]));
+// console.log(getTotal([3,4,5,6,7]));
+//create a object
+var user = {
+    firstName: "john",
+    lastName: "Weak",
+    role: "Software Engineer",
+    age: 40
+};
+console.log(user);
+var customer = {
+    name: "John Leo",
+    age: 30,
+    product: "Mobile",
+    address: "dhaka,bangladesh",
+    price: 30000
+};
+console.log(customer);
+function login(userData) {
+    return userData;
+}
+// console.log(login({ name: 'methon', age: 34, product: 'laptop', price: 123000 }));
+
+var userId = 'Mahfuz1232';
+// console.log(userId);
--- app.ts
+++ app.ts
@@ -1,18 +1,68 @@
-/*let num1=3;
-let num2=4;
-function calculate(num1:number,num2:number){
-  return num1+num2;
+// let num1=3;
+// let num2=4;
+// function calculate(num1:number,num2:number){
+//   return num1+num2;
+
+// }
+// console.log(calculate(num1,num2));
+
+
+
+function getTotal(numbers: Array<number>) {
+  return numbers.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0)
+}
+// console.log(getTotal([3,4,5,6,7]));
+
+
+
+//create a object
+
+const user = {
+
+  firstName: <string>"john",
+  lastName: <string>"Weak",
+  role: <string>"Software Engineer",
+  age: <number>40
+}
+console.log(user)
+
+
+//Type alias
+
+type Customer = {
+  name: string,
+  age: number,
+  product: string,
+  address?: string,
+  price: number
 
 }
-console.log(calculate(num1,num2));
-*/
 
-/*
-function getTotal(numbers:Array<number>){
-  return numbers.reduce((acc,curr)=>{
-    return acc+curr;
-  },0)
+const customer: Customer = {
+  name: "John Leo",
+  age: 30,
+  product: "Mobile",
+  address: "dhaka,bangladesh",
+  price: 30000
+
 }
-console.log(getTotal([3,4,5,6,7]));
 
-*/
+console.log(customer)
+
+
+
+function login(userData:Customer):Customer{
+
+  return userData;
+}
+
+// console.log(login({name:'methon',age:34,product:'laptop',price:123000}));
+
+type ID=number|string;
+
+const userId:ID='Mahfuz1232';
+console.log(userId)
+
+
